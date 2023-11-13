923e0fa5ba05211046e3e74044c1f853be9f5893
zhraaali <zhraa002.z.ali.2002@gmail.com>
2023-11-13 15:09:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calc from './components/Calc/Calc';
+
+function App() {
+  return (
+    <>
+      <Calc/>
+    </>
+  );
+}
+
+export default App;
